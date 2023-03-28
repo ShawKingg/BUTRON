93ee11df36669d517a8c3431d2adbf3c19d6b4ff
Shawn Kenneth Butron <123613029+ShawKingg@users.noreply.github.com>
2023-03-28 11:41:02 +0800

    Add files via upload

---
+++ BUTRON.php
@@ -0,0 +1,14 @@
+<?php
+
+          $Num=5;
+
+
+
+          for ($x =0; $x <= 10; $x++) {
+             $sum = $Num * $x;
+             echo "$Num x $x = $sum <br>";
+              
+           }
+
+
+?>
